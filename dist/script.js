2d6cf205d4257636231ff810d6ee566a432fa599
Miko <daxtersky@gmail.com>
2020-01-27 22:44:39 +0100

    prod version prepared

--- dist/script.js
+++ dist/script.js
@@ -91,8 +91,25 @@
 "use strict";
 
 
-document.addEventListener("DOMContentLoaded", function () {
+var _scriptAbc = __webpack_require__(1);
 
+var _scriptAbc2 = _interopRequireDefault(_scriptAbc);
+
+var _scriptAbc3 = __webpack_require__(2);
+
+var _scriptAbc4 = _interopRequireDefault(_scriptAbc3);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+document.addEventListener("DOMContentLoaded", function () {
+  getFirebaseData();
+  getUnsplashData();
+  getEmailService();
+
+  _scriptAbc4.default.testFetch();
+});
+
+var getFirebaseData = function getFirebaseData() {
   var config = {
     apiKey: "AIzaSyDq8sv5WWmiwbbj4RfA5-QkokqTh_uMblA",
     authDomain: "mikey-database.firebaseapp.com",
@@ -115,19 +132,107 @@ document.addEventListener("DOMContentLoaded", function () {
 
   dbRefObject.on('value', function (snap) {
     var mikeyDb = snap.val();
-    // console.log('mikeyDb', mikeyDb);
+    console.log('firebase database', mikeyDb);
     firebaseText.innerText = mikeyDb.textOnPage;
     firebaseText.style.color = 'red';
   });
 
   hobbies.on('child_added', function (snap) {
-    // console.log(snap.val());
+    console.log(snap.val());
     var li = document.createElement('li');
     li.innerText = snap.val();
     ulList.style.color = 'blue';
     ulList.appendChild(li);
   });
+};
+
+var getUnsplashData = function getUnsplashData() {
+  console.log('getUnsplashData');
+
+  console.log('------------------importedModule');
+  _scriptAbc2.default.importedFunction();
+  console.log('importedVar', _scriptAbc2.default.importedVar);
+  console.log('importedModule', _scriptAbc2.default);
+  console.log('------------------importedModule2');
+  _scriptAbc4.default.add(2, 4);
+  console.log('variable:', _scriptAbc4.default.catVar);
+  console.log('object:', _scriptAbc4.default.catObj.name, _scriptAbc4.default.catObj);
+  console.log('------------------');
+
+  // TODO fetch data from Unsplash
+  // https://api.unsplash.com/users/daxtersky?client_id=c1462734c4e2f7d399a2724533d58445eecb3b969c50f37262cc96c7f7acedc0
+  // https://api.unsplash.com/users/daxtersky/photos?client_id=c1462734c4e2f7d399a2724533d58445eecb3b969c50f37262cc96c7f7acedc0
+  // https://api.unsplash.com/users/daxtersky/collections?client_id=c1462734c4e2f7d399a2724533d58445eecb3b969c50f37262cc96c7f7acedc0
+  // TODO add referrals to image links: https://help.unsplash.com/en/articles/2511245-unsplash-api-guidelines
+};
+
+var getEmailService = function getEmailService() {
+  // TODO use google firebase as well? Not helloRoman example?
+  // https://medium.com/@edigleyssonsilva/cloud-functions-for-firebase-sending-e-mail-1f2631d1022e
+  // https://github.com/firebase/functions-samples/tree/master/quickstarts/email-users
+  // https://softauthor.com/send-email-with-firebase-cloud-functions-firestore-http-triggers
+  // https://techtrench.org/firebase-functions-send-email/
+};
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
 });
+exports.default = {
+  importedFunction: function importedFunction() {
+    console.log('imported function');
+  },
+  importedVar: 'imported variable'
+};
+
+
+var bar = 'baaar!';
+
+exports.bar = bar;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+function add(a, b) {
+  console.log(a, b, 'a+b', a + b);
+  // return a + b;
+}
+var catVar = 'string logged';
+var catObj = {
+  name: 'puszek'
+};
+
+function testFetch() {
+  var client_id = 'c1462734c4e2f7d399a2724533d58445eecb3b969c50f37262cc96c7f7acedc0';
+  var unsplashcall = 'https://api.unsplash.com/users/daxtersky/collections?client_id=' + client_id;
+  var unsplashUser = 'https://api.unsplash.com/users/daxtersky?client_id=c1462734c4e2f7d399a2724533d58445eecb3b969c50f37262cc96c7f7acedc0';
+  var unsplashUserPhotos = 'https://api.unsplash.com/users/daxtersky/photos?client_id=c1462734c4e2f7d399a2724533d58445eecb3b969c50f37262cc96c7f7acedc0';
+  var unsplashUserCollentions = 'https://api.unsplash.com/users/daxtersky/collections?client_id=c1462734c4e2f7d399a2724533d58445eecb3b969c50f37262cc96c7f7acedc0';
+  fetch(unsplashUserPhotos).then(function (resp) {
+    return resp.json();
+  }).then(function (response) {
+    // console.log('response', response);
+    console.log('photo', response[0].urls);
+    document.getElementById('unsplash-image').src = response[0].urls.regular;
+  }).catch(function (err) {
+    console.log('error', err);
+  });
+}
+
+exports.default = { add: add, catVar: catVar, catObj: catObj, testFetch: testFetch };
 
 /***/ })
 /******/ ]);
