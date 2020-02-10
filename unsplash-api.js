ff683f73a9e3d3bc5568cfd623c81e5e9201f4f0
Miko <daxtersky@gmail.com>
2020-02-10 23:45:23 +0100

    poc of parallax effect in pure css implemented

--- unsplash-api.js
+++ unsplash-api.js
@@ -4,6 +4,8 @@
 // https://api.unsplash.com/users/daxtersky/collections?client_id=c1462734c4e2f7d399a2724533d58445eecb3b969c50f37262cc96c7f7acedc0
 // TODO add referrals to image links: https://help.unsplash.com/en/articles/2511245-unsplash-api-guidelines
 
+
+
 function fetchUnsplashApi() {
   // https://unsplash.com/documentation
   const client_id = 'c1462734c4e2f7d399a2724533d58445eecb3b969c50f37262cc96c7f7acedc0';
@@ -11,10 +13,20 @@ function fetchUnsplashApi() {
   const unsplashUser =  'https://api.unsplash.com/users/daxtersky?client_id=c1462734c4e2f7d399a2724533d58445eecb3b969c50f37262cc96c7f7acedc0';
   const unsplashUserPhotos =  'https://api.unsplash.com/users/daxtersky/photos?client_id=c1462734c4e2f7d399a2724533d58445eecb3b969c50f37262cc96c7f7acedc0';
   const unsplashUserCollentions =  'https://api.unsplash.com/users/daxtersky/collections?client_id=c1462734c4e2f7d399a2724533d58445eecb3b969c50f37262cc96c7f7acedc0';
+  const unsplashMyCollention =  'https://api.unsplash.com/users/daxtersky/collection/9522596?client_id=c1462734c4e2f7d399a2724533d58445eecb3b969c50f37262cc96c7f7acedc0';
+  // fetch(unsplashUserPhotos)
+  //   .then(resp => resp.json())
+  //   .then(response => {
+  //     console.log('response', response);
+  //     console.log('photo', response[0].urls);
+  //     // document.getElementById('unsplash-image').src = response[0].urls.regular;
+  //   }).catch(err => {
+  //     console.log('error', err);
+  //   })
   fetch(unsplashUserPhotos)
     .then(resp => resp.json())
     .then(response => {
-      // console.log('response', response);
+      console.log('response', response);
       console.log('photo', response[0].urls);
       // document.getElementById('unsplash-image').src = response[0].urls.regular;
     }).catch(err => {
