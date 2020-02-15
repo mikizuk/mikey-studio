bdd28316a822762e917cb3e3c49ad547f5636a22
Miko <daxtersky@gmail.com>
2020-02-15 09:59:21 +0100

    aws lambda put in separate module

---
+++ awslambda-api.js
@@ -0,0 +1,43 @@
+const getAwsLambdaFunctions = () => {
+  console.log('aws lambda loading');
+  const form = document.getElementById('form');
+  const inputEmail = document.getElementById('inputEmail');
+  const inputMessage = document.getElementById('inputMessage');
+  if (form) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+      alert('Hurray! Mail was sent successfully! :)')
+      const sendEmail = 'https://f9akte6bf8.execute-api.eu-west-1.amazonaws.com/default/mikeystudio_send_email' + `?param1=${inputEmail.value}` + `&param2=${inputMessage.value}`;
+      const data = {
+        'param1': inputEmail.value,
+        'param2': inputMessage.value
+      };
+      console.log('data', data);
+
+      // TODO try handling aws functions' fetch with arrow funtions... maybe it'll help? point no. 5 https://dmitripavlutin.com/javascript-arrow-functions-best-practices/
+      fetch(sendEmail,
+        {
+          body: JSON.stringify(data),
+          // type: "POST",
+          method: "POST",
+          dataType: 'json',
+          mode: 'no-cors',
+          headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*"
+          },
+        })
+        .then(function (resp) {
+          console.log('resp', resp);
+          return resp.json();
+        })
+        .then(function (response) {
+          console.log('response', response);
+        })
+        .catch(function (err) {
+          console.log('error', err);
+        });
+    })
+
+  }
+}
--- script.js
+++ script.js
@@ -3,6 +3,7 @@ import importedModule from './script-abc.js';
 import importModuleTwo from './script-abc2.js';
 import unsplashApi from './unsplash-api.js';
 import firebaseApi from './firebase-api.js';
+import awsLambda from './awslambda-api.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   // 1
@@ -60,47 +61,7 @@ const getFirebaseData = () => firebaseApi.getFirebaseData();
 
 const getUnsplashData = () => unsplashApi.fetchUnsplashApi();
 
-const getEmailService = () => {
-  const form = document.getElementById('form');
-  const inputEmail = document.getElementById('inputEmail');
-  const inputMessage = document.getElementById('inputMessage');
-  if (form) {
-    form.addEventListener('submit', function (e) {
-      e.preventDefault();
-      alert('Hurray! Mail was sent successfully! :)')
-      const sendEmail = 'https://f9akte6bf8.execute-api.eu-west-1.amazonaws.com/default/mikeystudio_send_email' + `?param1=${inputEmail.value}` + `&param2=${inputMessage.value}`;
-      const data = {
-        'param1': inputEmail.value,
-        'param2': inputMessage.value
-      };
-      console.log('data', data);
-
-      fetch(sendEmail,
-        {
-          body: JSON.stringify(data),
-          // type: "POST",
-          method: "POST",
-          dataType: 'json',
-          mode: 'no-cors',
-          headers: {
-            'Content-Type': 'application/json',
-            "Access-Control-Allow-Origin": "*"
-          },
-        })
-        .then(function (resp) {
-          console.log('resp', resp);
-          return resp.json();
-        })
-        .then(function (response) {
-          console.log('response', response);
-        })
-        .catch(function (err) {
-          console.log('error', err);
-        });
-    })
-
-  }
-}
+const getEmailService = () => awsLambda.getAwsLambdaFunctions();
 
 const listenToObserver = () => {
   const navigation = document.querySelectorAll('.navigation')[0];
