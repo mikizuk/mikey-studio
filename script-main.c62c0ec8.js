a94db27a359c4c502d82927cc87e74db9c3a699b
Miko <daxtersky@gmail.com>
2020-04-13 12:37:54 +0200

    Updates

--- script-main.c62c0ec8.js
+++ script-main.c62c0ec8.js
@@ -755,7 +755,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64358" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64648" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
